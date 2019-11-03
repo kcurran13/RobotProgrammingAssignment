ac740df0359ee6afca3e966a10bf085f2b27144d
Kate <curran.kate.t@gmail.com>
2019-11-03 11:14:33 +0100

    added testing for robot

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 import robotProgram.entities.Robot;
+import robotProgram.steering.Command;
 import static robotProgram.steering.CardinalDirection.*;
+
 
 public class RobotTest {
     private Robot robot;
@@ -13,6 +18,28 @@ public class RobotTest {
 
     @Test
     void turnTest() {
+        robot.turn(Command.RIGHT);
+        assertSame(robot.getDirection(), EAST);
+
+        robot.turn(Command.LEFT);
+        robot.turn(Command.LEFT);
+        robot.turn(Command.LEFT);
+        assertSame(robot.getDirection(), SOUTH);
     }
 
+    @Test
+    void tryNewPositionTest() {
+        int startingX = robot.getX();
+        int startingY = robot.getY();
+        int[] newPos = robot.tryNewPosition();
+
+        assertEquals(startingY - 1, newPos[1]);
+
+        //check that trying a new position shouldn't actually SET a new position
+        robot.turn(Command.RIGHT);
+        robot.tryNewPosition();
+        robot.tryNewPosition();
+        newPos = robot.tryNewPosition();
+        assertEquals(startingX + 1, newPos[0]);
+    }
 }
